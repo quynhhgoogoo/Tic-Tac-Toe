0261f2e5d882332b4a96a9f93851c2ed20bb0dba
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2019-01-06 20:41:16 +0200

    bugs...

--- Ports/80x86/WIN32/VC/exercises/RTOS_1/rtos_1.c
+++ Ports/80x86/WIN32/VC/exercises/RTOS_1/rtos_1.c
@@ -3,7 +3,7 @@
 *                                                uC/OS-II
 *                                          The Real-Time Kernel
 *
-*					WIN32 PORT & LINUX PORT
+*					                    WIN32 PORT & LINUX PORT
 *                          (c) Copyright 2004, Werner.Zimmermann@fht-esslingen.de
 *                 (Similar to Example 1 of the 80x86 Real Mode port by Jean J. Labrosse)
 *                                           All Rights Reserved
@@ -35,9 +35,19 @@
 *********************************************************************************************************
 */
 
+typedef struct TaskCounter {
+    INT8U count1;
+    INT8U count2;
+} TaskCounter;
+
 OS_STK        TaskStk[N_TASKS][TASK_STK_SIZE];        /* Tasks stacks                                  */
 OS_STK        TaskStartStk[TASK_STK_SIZE];
+OS_EVENT      *RandomSem;
+OS_EVENT      *RanSem;
 char          TaskData[N_TASKS];                      /* Parameters to pass to each task               */
+char          board[HORIZONTAL][VERTICAL];
+INT8U         err;
+TaskCounter   count;
 
 /*
 *********************************************************************************************************
@@ -47,7 +57,13 @@ char          TaskData[N_TASKS];                      /* Parameters to pass to e
 
 void TaskStart(void *data);        /* Function prototype of Startup task           */
 void DrawBoard(char board[HORIZONTAL][VERTICAL]);
-void TicTacToe();
+void Player1(void *data);
+void Player2(void *data);
+void GameStatus(INT8U player, INT8U value);
+
+INT8U PositionCalculation(char board[3][3], INT8U selection, char input);
+INT8U GameLogic(char board[HORIZONTAL][VERTICAL]);
+void GameStatus(INT8U player, INT8U value)
 
 
 /*$PAGE*/
@@ -63,6 +79,12 @@ int  main(void)
 
     OSInit();                                              /* Initialize uC/OS-II                      */
 
+    RandomSem = OSSemCreate(1);
+    RanSem = OSSemCreate(2);
+
+    count.count1 = 0;
+    count.count2 = 0;
+
     OSTaskCreate(TaskStart, (void *)0, &TaskStartStk[TASK_STK_SIZE - 1], 0);
     OSStart();                                             /* Start multitasking                       */
     return 0;
@@ -74,17 +96,19 @@ int  main(void)
 *                                              STARTUP TASK
 *********************************************************************************************************
 */
-void  TaskStart(void *pdata)
+void TaskStart(void *pdata)
 {
     INT16S key;
     INT8U symbol;
 
 
     pdata = pdata;                                         /* Prevent compiler warning                 */
-    symbol = 'X';
 
-    OSTaskCreate(TicTacToe, (void *)&symbol, (void *)&TaskStk[0][TASK_STK_SIZE - 1], 1);
-    //OSTaskCreate(DrawBoard, (void *)&symbol, (void *)&TaskStk[0][TASK_STK_SIZE - 1], 2);
+    //OSTaskCreate(TicTacToe, (void *)&symbol, (void *)&TaskStk[0][TASK_STK_SIZE - 1], 1);
+    OSTaskCreate(DrawBoard, NULL, (void *)&TaskStk[0][TASK_STK_SIZE - 1], 1);
+    OSTaskCreate(Player1, (void *)&symbol, (void *)&TaskStk[0][TASK_STK_SIZE - 1], 2);
+    OSTaskCreate(Player2, (void *)&symbol, (void *)&TaskStk[0][TASK_STK_SIZE - 1], 3);
+    OSTaskCreate(GameStatus, NULL, (void *)&TaskStk[0][TASK_STK_SIZE - 1], 4);
 
     for (;;) {
 
@@ -146,6 +170,98 @@ void DrawBoard(char board[HORIZONTAL][VERTICAL])
     OSTimeDlyHMSM(0, 0, 1, 0);
 }
 
+void Player1(void *data){
+    int counter = 0;
+    int selection = 0;
+    int checker = 0;
+    char board[HORIZONTAL][VERTICAL];
+
+    while (counter != 9){
+    if (counter % 2 == 0)
+    {
+        printf("\nPlease input where you want to place X: ");
+        scanf("%d", &selection);
+
+    SCANX:
+        while (selection < 0 || selection > 10)
+        {
+            printf("Invalid input, please try again: ");
+            scanf("%d", &selection);
+        }
+
+        printf("\n");
+        checker = PositionCalculation(board, selection, 'x');
+
+        if (checker == -1)
+        {
+            selection = -1;
+            goto SCANX;
+        }
+
+        DrawBoard(board);
+        counter++;
+
+        if (counter > 3)
+        {
+            if (GameLogic(board) == 1)
+               GameStatus(1, 4);
+        }
+        
+    }
+}
+}
+
+void Player2(void *data){
+    int counter = 0;
+    int selection = 0;
+    int checker = 0;
+    char board[HORIZONTAL][VERTICAL];
+
+    while(counter != 9){
+        printf("\nPlease input where you want to place O: ");
+        scanf("%d", &selection);
+
+    SCANO:
+        while (selection < 0 || selection > 10)
+        {
+            printf("Invalid input, please try again: ");
+            scanf("%d", &selection);
+        }
+        printf("\n");
+
+        checker = PositionCalculation(board, selection, 'o');
+        if (checker == -1)
+        {
+            selection = -1;
+            goto SCANO;
+        }
+
+        DrawBoard(board);
+        counter++;
+
+        if (counter > 3)
+        {
+            if (GameLogic(board) == 1)
+                GameStatus(1,4);
+        }
+    }
+}
+
+void GameStatus(INT8U player, INT8U value){
+    OSSemPend(RandomSem, 0, &err);
+    if(player==1 && value > 3){
+        printf("X WINS");
+    }
+    if(player==2 && value > 3){
+        printf("O WINS");
+    }
+    else{
+        printf("Draw");
+    }
+    OSSemPost(RandomSem);
+}
+
+
 
 /*function code*/
 
@@ -156,11 +272,43 @@ void DrawBoard(char board[HORIZONTAL][VERTICAL])
 *********************************************************************************************************
 */
 
-void TicTacToe(){
-    INT8U selection;
-    INT8U counter;
-    INT8U checker;
 
-    char board[HORIZONTAL][VERTICAL];
-    DrawBoard(board);
+
+//Determine X and O position
+INT8U PositionCalculation(char board[3][3], INT8U selection, char input)
+{
+    INT8U column = selection % 3;
+    INT8U row = selection / 3;
+
+    if (column == 0)
+    {
+        column += 3;
+        row -= 1;
+    }
+
+    if (board[row][column - 1] == 'o' || board[row][column - 1] == 'x')
+        return -1;
+
+    board[row][column - 1] = input;
+    return 0;
+}
+
+//Win condition
+INT8U GameLogic(char board[HORIZONTAL][VERTICAL])
+{
+    INT8U i;
+    for (i = 0; i < HORIZONTAL; i++)
+    {
+        //Horizontal Check
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            return 1;
+
+        //VerticalCheck
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+            return 1;
+
+        //Diagonal Check
+        if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) || (board[0][2] == board[1][1] && board[1][1] == board[2][0]))
+            return 1;
+    }
 }
